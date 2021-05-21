b6984f344fccaabc11c1841c426af68e2143abb8
Nare-Stepanyan <narestepanyan90@gmail.com>
2021-05-21 19:07:10 +0300

    setup

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -1,6 +1,7 @@
 function request(url, method = "GET", body) {
   const config = {
     method: method,
+    credentials: "include",
     headers: {
       "Content-Type": "application/json",
     },
@@ -19,30 +20,3 @@ function request(url, method = "GET", body) {
     });
 }
 export default request;
-
-// function request(url, method = "GET", body) {
-//   const config = {
-//     method: method,
-//     mode: "cors",
-//     cache: "no-cache",
-//     credentials: "include",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     redirect: "follow",
-//     referrerPolicy: "no-referrer",
-//   };
-//   if (body) {
-//     config.body = JSON.stringify(body);
-//   }
-//   return fetch(url, config)
-//     .then((res) => res.json())
-//     .then((response) => {
-//       if (response.error) {
-//         throw response.error;
-//       }
-
-//       return response;
-//     });
-// }
-// export default request;

5cedab10a83cdd7a36de6db85856f335ab1aa073
Nare-Stepanyan <narestepanyan90@gmail.com>
2021-05-21 18:32:41 +0300

    setup

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -1,37 +1,9 @@
-// function request(url, method = "GET", body) {
-//   const config = {
-//     method: method,
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//   };
-//   if (body) {
-//     config.body = JSON.stringify(body);
-//   }
-//   return fetch(url, config)
-//     .then((res) => res.json())
-//     .then((response) => {
-//       if (response.error) {
-//         throw response.error;
-//       }
-
-//       return response;
-//     });
-// }
-// export default request;
-
 function request(url, method = "GET", body) {
   const config = {
-    method: method, // *GET, POST, PUT, DELETE, etc.
-    mode: "cors", // no-cors, *cors, same-origin
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "include", // include, *same-origin, omit
+    method: method,
     headers: {
       "Content-Type": "application/json",
-      // 'Content-Type': 'application/x-www-form-urlencoded',
     },
-    redirect: "follow", // manual, *follow, error
-    referrerPolicy: "no-referrer",
   };
   if (body) {
     config.body = JSON.stringify(body);
@@ -47,3 +19,30 @@ function request(url, method = "GET", body) {
     });
 }
 export default request;
+
+// function request(url, method = "GET", body) {
+//   const config = {
+//     method: method,
+//     mode: "cors",
+//     cache: "no-cache",
+//     credentials: "include",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     redirect: "follow",
+//     referrerPolicy: "no-referrer",
+//   };
+//   if (body) {
+//     config.body = JSON.stringify(body);
+//   }
+//   return fetch(url, config)
+//     .then((res) => res.json())
+//     .then((response) => {
+//       if (response.error) {
+//         throw response.error;
+//       }
+
+//       return response;
+//     });
+// }
+// export default request;

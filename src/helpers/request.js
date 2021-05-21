81e26801ebd544a30fe9c133b62b74fcbbfb9b73
Nare-Stepanyan <narestepanyan90@gmail.com>
2021-05-21 17:48:35 +0300

    setup

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -1,9 +1,37 @@
+// function request(url, method = "GET", body) {
+//   const config = {
+//     method: method,
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   };
+//   if (body) {
+//     config.body = JSON.stringify(body);
+//   }
+//   return fetch(url, config)
+//     .then((res) => res.json())
+//     .then((response) => {
+//       if (response.error) {
+//         throw response.error;
+//       }
+
+//       return response;
+//     });
+// }
+// export default request;
+
 function request(url, method = "GET", body) {
   const config = {
-    method: method,
+    method: method, // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
     headers: {
       "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
     },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer",
   };
   if (body) {
     config.body = JSON.stringify(body);
